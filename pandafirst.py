9bebba056f46bfaf7204c3045a843b7515d73c68
Rohan2519Y <rohankumar17645@gmail.com>
2026-06-28 10:58:09 +0530

    first commit

---
+++ pandafirst.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+# S = pd.Series([1, 2, 3, 4, 5])
+# S = pd.Series(['A', 'B', 'C', 'D'], index = [100, 200, 300, 400])
+# A = np.array(['Ha', 'Pe', 'Th', 'Vi'])
+# A = {'100':'Tanmay', '200':'Bhagel', '300':'Saksham', '400':'Harsh'}
+# S = pd.Series(A)
+# S.index = (1, 2, 3, 4)
+# S.name = 'People'
+# print(S)
+# print(S.values)
+# print(S.index)
+# print(S.size)
+# print(S.empty)
+# print(S.head(2))
+# print(S.tail(2))
+# print(S.count())
+# print(S[['100', '200']])
+
+# S1 = pd.Series([1, 2, 3, 4])
+# S2 = pd.Series([5, 6, 7, 8])
+S1 = pd.Series([1, 2, 3, 4], index = ['A', 'B', 'C', 'D'])
+S2 = pd.Series([5, 6, 7, 8], index = ['A', 'C', 'F', 'G'])
+# print(S1+S2)
+print(S1.add(S2, fill_value=0))
+print(S1.sub(S2, fill_value=0))
