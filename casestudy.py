3299cc282290695965aafa10ed02330fd0b9a530
Rohan2519Y <rohankumar17645@gmail.com>
2026-07-24 19:19:19 +0530

    Update

--- casestudy.py
+++ casestudy.py
@@ -18,8 +18,8 @@ Transaction = pd.DataFrame(pd.read_csv('F:\Pandas\Transactions.csv'))
 
 Temp_Merge = pd.merge(Transaction, Customers, how='inner', left_on=['cust_id'], right_on=['customer_Id'])
 Customer_Final = pd.merge(Product_Hierarchy, Temp_Merge, how='inner', left_on=['prod_cat_code'], right_on=['prod_cat_code'])
-# print(Customer_Final)
-# print(Customer_Final.columns)
+print(Customer_Final)
+print(Customer_Final.columns)
 
 
 
@@ -52,6 +52,63 @@ Customer_Final = pd.merge(Product_Hierarchy, Temp_Merge, how='inner', left_on=['
 
 
 # 3 - Generate histograms for all continuous variables and frequency bars for categorical variables.
-Customer_Final['city_code'].hist()
-plt.title("City Code")
-plt.show()
+# Customer_Final['city_code'].hist()
+# plt.title("City Code")
+# plt.show()
+# Customer_Final['city_code'].value_counts().plot(kind='bar')
+# plt.show()
+
+
+# Customer_Final['prod_cat'].hist()
+# plt.title("Product Category")
+# plt.show()
+# Customer_Final['prod_cat'].value_counts().plot(kind='bar')
+# plt.show()
+
+
+# Customer_Final['prod_subcat'].hist()
+# plt.title("Product SubCategory")
+# plt.show()
+# Customer_Final['prod_subcat'].value_counts().plot(kind='bar')
+# plt.show()
+
+
+# Customer_Final['Store_type'].hist()
+# plt.title("Store Type")
+# plt.show()
+# Customer_Final['Store_type'].value_counts().plot(kind='bar')
+# plt.show()
+
+
+
+
+
+# 4 - Calculate the following information using the merged dataset :
+#       A - Time period of the available transaction data
+# Customer_Final['Time_Peroid'] = pd.Timestamp.now() - pd.to_datetime(Customer_Final['tran_date'], dayfirst=True, format='mixed')
+# print(Customer_Final['Time_Period])
+
+
+#       B - Count of transactions where the total amount of transaction was negative
+# print("Count", Customer_Final.loc[(Customer_Final['total_amt']) < 0,'total_amt'].count())
+
+
+
+
+# 5 - Analyze which product categories are more popular among females vs male customers.
+# print(pd.crosstab(index=Customer_Final['Gender'], columns=Customer_Final['prod_cat']).idxmax(axis=1))
+
+
+
+
+# 6 - Which City code has the maximum customers and what was the percentage of customers from that city?
+# city = Customer_Final.groupby("city_code")["cust_id"].nunique()
+
+# print("City Code:", city.idxmax())
+# print("Percentage:", (city.max() / city.sum()) * 100)
+
+
+
+
+# 7 - Which store type sells the maximum products by value and by quantity?
+# print(Customer_Final.groupby("Store_type")['Qty'].sum().idxmax())
