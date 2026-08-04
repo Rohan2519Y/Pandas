b3ec204ba40525816c70add945633ba1c489ddc4
Rohan2519Y <rohankumar17645@gmail.com>
2026-08-04 18:09:21 +0530

    Case Study 2

---
+++ casestudy2/casestudy2.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+pd.set_option('display.max_columns', None)
+
+Customer_Acquisition = pd.read_csv('F:/Pandas/casestudy2/Customer Acqusition.csv')
+Spend = pd.read_csv('F:/Pandas/casestudy2/spend.csv')
+Repayment = pd.read_csv('F:/Pandas/casestudy2/Repayment.csv')
+
+Full_Merge = pd.merge(Repayment, (pd.merge(Customer_Acquisition, Spend, on=['Customer'])), on=['Customer'], suffixes=('_Repay', '_Spend'))
+
+
+# print(Customer_Acquisition)
+# print(Spend)
+# print(Repayment)
+
+print(Full_Merge)
+
+# 1. In the above dataset
+#           A - In case age is less than 18, replace it with mean of age values
+
+# MeanAge = Customer_Acquisition.loc[Customer_Acquisition['Age'] < 18, 'Age'].mean()
+# Customer_Acquisition.loc[Customer_Acquisition['Age'] < 18, 'Age'] = int(MeanAge)
+# print(Customer_Acquisition)
+
+
+#           B - In case spend amount is more than the limit, replace it with 50% of that customer’s limit.  
+#           (customer’s limit provided in acquisition table is the per transaction limit on his card)
+
+# Full_Merge['Final_Limit'] = Full_Merge.loc[Full_Merge['Amount_Spend'] > Full_Merge['Limit']]['Limit'] * 0.5
+# Full_Merge['Final_Limit'] = Full_Merge['Final_Limit'].fillna(0)
+# print(Full_Merge.loc[ :,['Limit', 'Final_Limit']])
+
+
+#           C - Incase the repayment amount is more than the limit, replace the repayment with the limit.
+
+Full_Merge.loc[Full_Merge['Amount_Repay'] > Full_Merge['Limit'], 'Amount_Repay'] = Full_Merge['Limit']
+print(Full_Merge.loc[Full_Merge['Amount_Repay'] > Full_Merge['Limit']]['Limit'])
